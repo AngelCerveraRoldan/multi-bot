8b62cd6e15c24525b3e19c329e8c2525f227fe2c
acroldann <acroldann@gmail.com>
2021-07-11 16:16:08 +0100

    Command manager replaced

--- main.py
+++ main.py
@@ -1,38 +1,27 @@
 ## Interlan Imports
 from src import wiki
-from src import command_manager
 
 ## External Imports
 import os
-import discord
-from dotenv import load_dotenv
+import random
 
-## Discord Bot
+from discord.ext import commands
+from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-client = discord.Client()
+bot = commands.Bot(command_prefix='$')
 
-## This will print when the bot is ready to be used
-@client.event
-async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+@bot.command(name='search', help='Use this command to look something up in wikipedia.')
+async def search(ctx, *sentence):
+    ## If we added another argument between ctx and *sentence, then sentence would be the every word after the third. This allows us to have multiple arguments.
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    call, text = command_manager.change(message.content)
+    sentence = " ".join(sentence)
     
-    print(f'CALL HERE: {call}')
-    print(f'TEXT HERE: {text}')
+    print(sentence)
 
-    if call == '$s':
-        response = wiki.summary(text)
+    response = wiki.summary(sentence)
+    await ctx.send(response)
 
-        await message.channel.send(response)
-
-
-client.run(TOKEN)
+bot.run(TOKEN)
--- src/command_manager.py
+++
@@ -1,9 +0,0 @@
-## Return the the first word and the rest of the sentence all in lowe case
-def change(sentence):
-    sentence = sentence.split()
-
-    word = sentence[0]
-    rest = ' '.join(sentence[1:])
-    
-    return word, rest
-
